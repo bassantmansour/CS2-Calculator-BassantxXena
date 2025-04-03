c8b75600109391d2f312259907eb5c671fa4884d
Xena <xenaragy@gmail.com>
2025-04-03 22:53:15 +0000

    adding header and calculator.cpp files, 2nd commit

---
+++ calculator.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "Calculator.h"
+using namespace std;
+
+Calculator::Calculator() : r(false) {}
+double Calculator::add(double a, double b) const {return a + b;}
+double Calculator::subtract(double a, double b) const {return a - b;}
+double Calculator::multiply(double a, double b) const {return a * b;}
+
+
+double Calculator::divide(double a, double b) const {
+    if (b == 0) {
+       cout << "Division by zero is not allowed" << endl;
+       return 1;
+    }
+    return a / b;
+}
+long long Calculator::factorial(int n) const {
+    if (n < 0) {
+        cout << "Number can't be negative" << endl;
+        return 1;
+    }
+    long result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+int Calculator::gcd(int a, int b) const {
+
+    a = abs(a);
+    b = abs(b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int Calculator::lcm(int a, int b) const {
+
+    a = abs(a);
+    b = abs(b);
+    // If one of the inputs is 0
+    if (a == 0 || b == 0) {
+        return 0;
+    }
+    // LCM is (a * b) / gcd(a, b)
+    return ((a * b) / gcd(a, b));
+}
+int Calculator::random(int min, int max) {
+    if (min > max) {
+        cout << "Minimum value must be less than max value" << endl;
+        return 1;
+    }
+    if (!r) {
+        srand(time(0));
+        r = true;
+    }
+    // Generate random number within range
+    return min + rand() % (max - min + 1);
+}
+
+
---
+++ calculator.h
@@ -0,0 +1,21 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+private:
+    bool r; //for random number generator
+
+public:
+    Calculator(); //constructor
+    double add(double a, double b) const;
+    double subtract(double a, double b) const;
+    double multiply(double a, double b) const;
+    double divide(double a, double b) const;
+    long long factorial(int n) const;
+    int gcd(int a, int b) const;
+    int lcm(int a, int b) const;
+    int random(int min, int max);
+};
+
+#endif CALCULATOR_H
+
